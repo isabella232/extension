e320797ad91884464873b4de8a61961b2d545a4c
Remon Nashid <remon.sherin@gmail.com>
2020-09-15 11:58:40 -0700

    Add accounts selector

---
+++ packages/extension-polymath/store/selectors.ts
@@ -0,0 +1,35 @@
+import { createSelector } from '@reduxjs/toolkit';
+import { DidType } from '../types';
+import { RootState } from './rootReducer';
+
+export const didsList = createSelector(
+  (state: RootState) => state.identities,
+  (identities) => Object.keys(identities)
+);
+
+type ReversedDidList = Record<string, {did: string, keyType: DidType, cdd: boolean | undefined}>;
+
+export const reversedDidList = createSelector(
+  (state: RootState) => state.identities,
+  (identities): ReversedDidList => {
+    return Object.keys(identities).reduce((reversedList: ReversedDidList, did) => {
+      const identity = identities[did];
+
+      reversedList[identity.priKey] = { cdd: identity.cdd, did, keyType: DidType.primary };
+
+      identity.secKeys.forEach((secKey) => {
+        reversedList[secKey] = { cdd: identity.cdd, did, keyType: DidType.secondary };
+      });
+
+      return reversedList;
+    }, {} as ReversedDidList);
+  }
+);
+
+export const identifiedAccounts = createSelector(
+  (state: RootState) => state.accounts,
+  reversedDidList,
+  (accounts, reversedDidList: ReversedDidList) => {
+    return Object.values(accounts).map((account) => ({ ...account, ...reversedDidList[account.address] }));
+  }
+);
