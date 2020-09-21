511680deb2268a6107dc5c971b5daf90bd8b5f15
Bilal Alghazi <balghazi@gmail.com>
2020-09-21 16:38:47 -0400

    feat: edit account name

--- packages/ui/src/Popup/Accounts/AccountView.tsx
+++ packages/ui/src/Popup/Accounts/AccountView.tsx
@@ -1,15 +1,16 @@
-import React, { FC } from 'react';
+import React, { FC, useState, useContext } from 'react';
 import { IdentifiedAccount } from '@polymath/extension-core/types';
 import { formatters } from "../../util";
-import { Box, Text, TextEllipsis, Flex, Icon, StatusBadge } from '../../ui';
+import { Box, Text, TextEllipsis, Flex, Icon, StatusBadge, TextInput } from '../../ui';
 import {
   SvgAccount,
-  SvgCheck,
   SvgCheckboxMarkedCircle,
-  SvgDotsVertical,
+  SvgPencilOutline,
   SvgWindowClose,
-  SvgAlertCircle,
+  SvgCheck,
 } from "@polymath/ui/assets/images/icons";
+import { editAccount } from "../../messaging";
+import { ActionContext } from "../../components";
 
 export interface Props {
   account: IdentifiedAccount;
@@ -18,13 +19,41 @@ export interface Props {
 }
 
 export const AccountView: FC<Props> = ({account, isAssigned, isSelected}) => {
+  const { name, address } = account;
+
+  const onAction = useContext(ActionContext);
+
+  const [isEditing, setEditing] = useState(false);
+  const [newName, setNewName] = useState(name);
+
   const renderType = (keyType: string) => {
     const color = keyType === 'primary' ? 'green' : 'blue';
     const text = keyType === 'primary' ? 'Master' : 'Secondary';
 
     return (
-      <StatusBadge variant={color}>{text}</StatusBadge>
+      !isEditing && <StatusBadge variant={color}>{text}</StatusBadge>
     );
+  };
+
+  const cancelEditing = () => {
+    setEditing(false);
+  };
+
+  const editName = () => {
+    setEditing(true);
+  };
+
+  const handleNameChange = (e) => {
+    setNewName(e.target.value);
+  };
+
+  const save = () => {
+    editAccount(address, newName)
+      .then(() => {
+        onAction();
+        setEditing(false);
+      })
+      .catch(console.error);
   };
 
   return (
@@ -39,7 +68,7 @@ export const AccountView: FC<Props> = ({account, isAssigned, isSelected}) => {
             width={32}
           >
             <Flex justifyContent="center" pt="xs">
-              <Text variant="b2m" color="brandMain">{account.name.substr(0,1)}</Text>
+              <Text variant="b2m" color="brandMain">{name.substr(0,1)}</Text>
             </Flex>
           </Box>
         </Box>
@@ -49,9 +78,27 @@ export const AccountView: FC<Props> = ({account, isAssigned, isSelected}) => {
             flexDirection="row"
           >
             <Flex flexDirection="row">
-              <Text variant="b2m" color="gray.1">
-                {account.name}
-              </Text>
+              {isEditing && (
+                <Flex flexDirection="row">
+                  <TextInput defaultValue={name} value={newName} onChange={handleNameChange} />
+                  <Box ml="xs">
+                    <Icon Asset={SvgCheck} width={16} height={16} color="gray.2" onClick={save} />
+                  </Box>
+                  <Box ml="xs">
+                    <Icon Asset={SvgWindowClose} width={16} height={16} color="gray.2" onClick={cancelEditing} />
+                  </Box>
+                </Flex>
+              )}
+              {!isEditing && (
+                <Flex flexDirection="row">
+                  <Text variant="b2m" color="gray.1">
+                    {name}
+                  </Text>
+                  <Box ml="xs">
+                    <Icon Asset={SvgPencilOutline} width={16} height={16} color="gray.2" onClick={editName} />
+                  </Box>
+                </Flex>
+              )}
               <Box ml="s">
                 {renderType(account.keyType)}
               </Box>
@@ -65,7 +112,7 @@ export const AccountView: FC<Props> = ({account, isAssigned, isSelected}) => {
             <Box>
               <Text variant="b3" color="gray.3">
                 <TextEllipsis size={13}>
-                  {account.address}
+                  {address}
                 </TextEllipsis>
               </Text>
             </Box>
