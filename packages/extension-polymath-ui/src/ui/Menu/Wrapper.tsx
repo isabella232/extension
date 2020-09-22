c96136505bc4f22e0a44739ecf1a4814a1692efe
Remon Nashid <remon.sherin@gmail.com>
2020-09-22 15:19:34 -0700

    Add basic typing to the custom menu component

--- packages/extension-polymath-ui/src/ui/Menu/Wrapper.tsx
+++ packages/extension-polymath-ui/src/ui/Menu/Wrapper.tsx
@@ -2,11 +2,12 @@ import React from 'react';
 import { Wrapper as ScWrapper } from 'react-aria-menubutton';
 import { styled } from '@polymath/extension-ui/styles';
 
-export interface WrapperProps {
+type Props = React.ComponentProps<typeof ScWrapper>;
+export interface WrapperProps extends Props {
   onSelection: () => void;
 }
 
-export const Wrapper = styled(ScWrapper)({
+export const Wrapper: React.ReactElement<WrapperProps> = styled(ScWrapper)({
   position: 'relative',
   display: 'inline-block'
 });
